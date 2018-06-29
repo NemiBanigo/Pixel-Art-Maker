32fa68079d895cfb3704d2d292ba4fa5965a3945
Nemi Banigo <nemi@Nemis-MacBook-Air.local>
2018-06-29 12:10:32 +0100

    first commit

---
+++ pixel.js
@@ -0,0 +1,11 @@
+let setColor = document.querySelector('#colorPicker');
+console.log(setColor);// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+    // Your code goes here!
+
+}
