b2b62be18445937659607fab10758a174df34d29
Nemi Banigo <nemi@Nemis-MacBook-Air.local>
2018-06-30 11:33:35 +0100

    Add jQuery function

--- pixel.js
+++ pixel.js
@@ -1,11 +1,19 @@
-let setColor = document.querySelector('#colorPicker');
-console.log(setColor);// Select color input
-// Select size input
+jQuery(function () {
+    let height;
+    width;
+    color;
 
-// When size is submitted by the user, call makeGrid()
+    $('#sizePicker').submit()
 
-function makeGrid() {
 
-    // Your code goes here!
+    // Select color input
+    // Select size input
 
-}
+    // When size is submitted by the user, call makeGrid()
+
+    function makeGrid() {
+
+        // Your code goes here!
+
+    }
+});
